3da26f2b6fcb971461eeaf6b4f698377e1a44188
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-05-26 12:08:50 -0400

    added other boundary tests for 0 items and many items, removed mutation from initial test

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,10 +1,26 @@
 const {Shop, Item} = require("../src/gilded_rose");
 const {shopItems} = require("./texttest_fixture.js");
 
+//boundary cases
+
+describe("Zero items in shop", function(){
+  it("should have zero items in shop", function() {
+    const gildedRose = new Shop([])
+    expect(gildedRose.items.length).toBe(0)
+  })
+})
+
 describe("Single item in shop", function() {
   it("should contain an item named foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
+    expect(gildedRose.items[0].name).toBe("foo");
   });
 });
+
+describe("Many items in a shop", function() {
+  it("should contain the number of items in the shopItems array", function() {
+    const gildedRose = new Shop(shopItems)
+    expect (gildedRose.items.length).toBe(shopItems.length)
+  })
+})
+
