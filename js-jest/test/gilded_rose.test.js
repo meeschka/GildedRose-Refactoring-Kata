d6e29a247236b9a9a0b802eb6e236e564be4bf68
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-05-26 13:13:44 -0400

    added mutation tests for quality and sellIn date

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -24,3 +24,46 @@ describe("Many items in a shop", function() {
   })
 })
 
+//mutations
+//item quality decreases
+describe("item quality decreases after 1 day", function() {
+  it("should decrease after quality updated", function() {
+    const gildedRose = new Shop([new Item("foo", 2, 2)])
+    const items = gildedRose.updateQuality()
+    expect (items[0].quality).toBe(1)
+  })
+})
+
+describe("item quality decreases after 2 days", function() {
+  it("should decrease after quality updated", function() {
+    const gildedRose = new Shop([new Item("foo", 2, 2)])
+    gildedRose.updateQuality()
+    const items = gildedRose.updateQuality()
+    expect (items[0].quality).toBe(0)
+  })
+})
+
+describe("item quality should not decrease below 0", function() {
+  it("should stop at quality = 0 ", function() {
+    const gildedRose = new Shop([new Item("foo", 2, 0)])
+    const items = gildedRose.updateQuality()
+    expect (items[0].quality).toBe(0)
+  })
+})
+
+describe("item quality should decrease twice as fast after sellIn date", function() {
+  it("should decrease by two", function() {
+    const gildedRose = new Shop([new Item("foo", 0, 2)])
+    const items = gildedRose.updateQuality()
+    expect (items[0].quality).toBe(0)
+  })
+})
+
+//item sellIn decreases
+describe("item sellin decreases after 1 day", function() {
+  it("should decrease after quality updated", function() {
+    const gildedRose = new Shop([new Item("foo", 2, 2)])
+    const items = gildedRose.updateQuality()
+    expect (items[0].sellIn).toBe(1)
+  })
+})
