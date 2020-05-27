e70056df78be7d13b552c407d8e0e254ce4d79aa
Michelle Pitts <michelle.m.pitts@gmail.com>
2020-05-27 09:33:06 -0400

    refactored tests based on further research into jest

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,155 +1,158 @@
 const {Shop, Item} = require("../src/gilded_rose");
 const {shopItems} = require("./texttest_fixture.js");
+//consider breaking into interface and methods
+//what if the item is bad? will it do multiple items? grep?
+//break it into describes and it
+//use fixture more
+//desc ribe a shop with one item, with multiple items, thats one way to break it down
+
+
 
 //boundary cases
 
-describe("Zero items in shop", function(){
+describe("Shop component", function(){
   it("should have zero items in shop", function() {
     const gildedRose = new Shop([])
     expect(gildedRose.items.length).toBe(0)
   })
-})
 
-describe("Single item in shop", function() {
   it("should contain an item named foo", function() {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     expect(gildedRose.items[0].name).toBe("foo");
   });
-});
 
-describe("Many items in a shop", function() {
   it("should contain the number of items in the shopItems array", function() {
     const gildedRose = new Shop(shopItems)
     expect (gildedRose.items.length).toBe(shopItems.length)
   })
-})
 
-//mutations
-//item quality decreases
-describe("item quality decreases after 1 day", function() {
-  it("should decrease after quality updated", function() {
-    const gildedRose = new Shop([new Item("foo", 2, 2)])
-    const items = gildedRose.updateQuality()
-    expect (items[0].quality).toBe(1)
-  })
-})
-
-describe("item quality decreases after 2 days", function() {
-  it("should decrease after quality updated", function() {
-    const gildedRose = new Shop([new Item("foo", 2, 2)])
-    gildedRose.updateQuality()
-    const items = gildedRose.updateQuality()
-    expect (items[0].quality).toBe(0)
-  })
-})
-
-describe("item quality should not decrease below 0", function() {
-  it("should stop at quality = 0 ", function() {
-    const gildedRose = new Shop([new Item("foo", 2, 0)])
-    const items = gildedRose.updateQuality()
-    expect (items[0].quality).toBe(0)
-  })
-})
-
-describe("item quality should decrease twice as fast after sellIn date", function() {
-  it("should decrease by two", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 2)])
-    const items = gildedRose.updateQuality()
-    expect (items[0].quality).toBe(0)
-  })
-})
-
-//fails - not sure if this is a bug, or if I've misunderstood the way the component works. To look at further.
-describe("item quality should not be above 50", function() {
-  it("should be 50", function() {
+  //assume that items added with quality > 50 are revised down to 50
+  it("should not contain an item with quality > 50", function() {
     const gildedRose = new Shop([new Item("Foo", 2, 55)])
-    const items = gildedRose.updateQuality()
     expect(items[0].quality).toBe(50)
   })
 })
 
-//item sellIn decreases
-describe("item sellin decreases after 1 day", function() {
-  it("should decrease after quality updated", function() {
-    const gildedRose = new Shop([new Item("foo", 2, 2)])
-    const items = gildedRose.updateQuality()
-    expect (items[0].sellIn).toBe(1)
+describe("updateQuality method", function() {
+  describe("changes to quality", function() {
+    const gildedRose = new Shop([new Item("foo", 2, 4), new Item("bar", 5, 10)])
+
+    it("should decrease after quality updated", function() {
+      const items = gildedRose.updateQuality()
+      expect (items[0].quality).toBe(3)
+    })
+  
+    it("should decrease after quality updated a second time", function() {
+      const items = gildedRose.updateQuality()
+      expect (items[0].quality).toBe(2)
+    })
+
+    if("should also decrease the quality of the second item", function() {
+      expect (item[1].quality.toBe(8))
+    })
+  
+    it("should decrease by two after sellIn date", function() {
+      const items = gildedRose.updateQuality()
+      expect (items[0].quality).toBe(0)
+    })
+  
+    it("should stop at quality = 0 ", function() {
+      const items = gildedRose.updateQuality()
+      expect (items[0].quality).toBe(0)
+    })
   })
-})
 
-describe("item sellin can be less than 0", function() {
-  it("should decrease after quality updated", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 2)])
-    const items = gildedRose.updateQuality()
-    expect (items[0].sellIn).toBe(-1)
+  describe("changes to sellIn date", function() {
+    const gildedRose = new Shop([new Item("foo", 1, 2)])
+
+    it("should decrease after quality updated", function() {
+      const items = gildedRose.updateQuality()
+      expect (items[0].sellIn).toBe(0)
+    })
+
+    it("should be capable of negative numbers", function() {
+      const items = gildedRose.updateQuality()
+      expect (items[0].sellIn).toBe(-1)
+    })
   })
-})
 
-//special cases
-describe("aged brie increases in quality over time", function() {
-  it("should increase in quality", function() {
-    const gildedRose = new Shop([new Item("Aged Brie", 2, 0)])
-    const items = gildedRose.updateQuality()
-    expect(items[0].quality).toBe(1)
-  })
-})
+  describe("special cases", function() {
+    describe("aged brie", function() {
 
-describe("aged brie increases in quality over time after sellIn date", function() {
-  it("should increase in quality", function() {
-    const gildedRose = new Shop([new Item("Aged Brie", -1, 0)])
-    const items = gildedRose.updateQuality()
-    expect(items[0].quality).toBe(2)
-  })
-})
+      const gildedRose = new Shop([new Item("Aged Brie", 1, 48)])
 
-describe("item quality should not be above 50 - aged brie", function() {
-  it("should be 50", function() {
-    const gildedRose = new Shop([new Item("Aged Brie", 2, 50)])
-    const items = gildedRose.updateQuality()
-    expect(items[0].quality).toBe(50)
-  })
-})
+      it("should increase in quality", function() {
+        const items = gildedRose.updateQuality()
+        expect(items[0].quality).toBe(49)
+      })
 
-describe("sulfuras should always have a quality of 80", function() {
-  it("should be 80", function() {
-    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", -1, 80)])
-    const items = gildedRose.updateQuality()
-    expect(items[0].quality).toBe(80)
-  })
-})
+      //as written, aged brie increases in quality at twice the base rate after sellIn date
+      //it is unclear if this is the intended behaviour
+      //based on the rules 'Once the sell by date has passed, Quality degrades twice as fast' and
+      //'"Aged Brie" actually increases in Quality the older it gets', as well as the assumed linear nature of cheese aging,
+      //I've elected for the rate of cheese quality increase to remain constant, even after the sellIn date
+      it("should increase in quality after sellIn date", function() {
+        const items = gildedRose.updateQuality()
+        expect(items[0].quality).toBe(50)
+      })
 
-//the specs say that "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-//as we are not able to change the item component, we must give this item some sellIn date
-//to denote that we do not need to sell it, do we want to just never update it? Or update and never look at it?
+      it("should not increase above 50", function() {
+        const items = gildedRose.updateQuality()
+        expect(items[0].quality).toBe(50)
+      })
+    })
 
-// describe("sulfuras sellIn date should not change", function() {
-//   it("should be 1", function() {
-//     const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 1, 80)])
-//     const items = gildedRose.updateQuality()
-//     expect(items[0].sellIn).toBe(1)
-//   })
-// })
+    describe("sulfuras", function() {
+      const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 1, 80)])
 
-describe("concert ticket quality should increase by 2 when there are <10 days before sellIn", function() {
-  it("should be 1", function() {
-    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 9, 44)])
-    const items = gildedRose.updateQuality()
-    expect(items[0].quality).toBe(46)
-  })
-})
+      it("should be 80 before sellIn date", function() {
+        const items = gildedRose.updateQuality()
+        expect(items[0].quality).toBe(80)
+      })
 
-describe("concert ticket quality should increase by 3 when there are <5 days before sellIn", function() {
-  it("should be 1", function() {
-    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 4, 44)])
-    const items = gildedRose.updateQuality()
-    expect(items[0].quality).toBe(47)
-  })
-})
+      it("should be 80 after sellIn date", function() {
+        const items = gildedRose.updateQuality()
+        expect(items[0].quality).toBe(80)
+      })
 
-describe("concert ticket quality should be 0 after sellIn date", function() {
-  it("should be 1", function() {
-    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49)])
-    const items = gildedRose.updateQuality()
-    expect(items[0].quality).toBe(0)
+      //the specs say that "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+      //as we are not able to change the item component, we must give this item some sellIn date
+      //to denote that we do not need to sell it, do we want to just never update it? Or update and never look at it?
+    })
+
+    describe("concert tickets", function(){
+
+      const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 11, 30)])
+      it("should increase by 1 when there are >10 days left", function() {
+        const items = gildedRose.updateQuality()
+        expect(items[0].quality).toBe(31)
+      })
+      
+      it("should increase by 2 when there are 6-10 days left", function() {
+        const items = gildedRose.updateQuality()
+        expect(items[0].quality).toBe(33)
+      })
+
+      it("should increase by 3 when there are 0-5 days before sellIn", function() {
+        for (let i =0; i<4; i++){
+          gildedRose.updateQuality()
+        }
+        const items = gildedRose.updateQuality()
+        expect(items[0].quality).toBe(33+4*2+3)
+      })
+
+      it("should not increase above 50", function() {
+        for (let i =0; i<3; i++){
+          gildedRose.updateQuality()
+        }
+        const items = gildedRose.updateQuality()
+        expect(items[0].quality).toBe(50)
+      })
+
+      it("concert ticket quality should be 0 after sellIn date", function() {
+        const items = gildedRose.updateQuality()
+        expect(items[0].quality).toBe(0)
+      })
+    })
   })
 })
